b4476e502432db30ef5dd102ab80aefa2d6d4b66
fouadsan <F.benayad95@gmail.com>
2021-10-19 19:14:26 +0100

    Finalized movie-db project

---
+++ 21-movie-db/setup/src/Error.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Error() {
+    return (
+        <main>
+            <div>
+                <h2>Error</h2>
+            </div>
+        </main>
+        
+    )
+}
+
+export default Error
